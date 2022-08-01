021215ecbf77560d6817a5e36244afa9b02d3393
BilliDaGOAT <96752749+BilliDaGOAT@users.noreply.github.com>
2022-08-01 09:37:03 -0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ import Mint from '../components/Mint';
 import Faq from '../components/Faq';
 import Team from '../components/Team';
 import Roadmap from '../components/Roadmap';
-import topImage from '../public/assets/TheDropNFTVIDCARDBG.jpeg';
+import topImage from '../public/assets/TheDropNFTVIDCARDBG.jpg';
 import contractConfig from '../config/contract-config.json';
 
 const Home: NextPage = () => {
@@ -27,13 +27,7 @@ const Home: NextPage = () => {
           <h1 className="text-5xl font-bold mb-4">{nftName}</h1>
 
           <p className="text-xl">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
+           
           </p>
         </Prose>
       </div>
